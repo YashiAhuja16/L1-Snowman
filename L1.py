eb4bb4255b8d8f3453731bfb6210c5b428a22b74
YashiAhuja16 <yashi.ahuja16@gmail.com>
2025-01-07 20:10:33 -0800

    Add files via upload

---
+++ L1.py
@@ -0,0 +1,23 @@
+import turtle as t 
+t.bgcolor ("#89CFF0") # background color 
+
+t.width (4)
+
+t.color("#F4C2C2", "white ") 
+t.begin_fill()
+for y in range (36): #bottom circle
+    t.forward (20)
+    t.right(10)
+t.end_fill ()
+
+
+t.color("#F4C2C2", "white ") 
+t.begin_fill()
+for y in range (36): #bottom circle
+    t.forward (15)
+    t.left(10)
+t.end_fill ()
+
+t.mainloop ()# to hold the turtle screen 
+    
+    
